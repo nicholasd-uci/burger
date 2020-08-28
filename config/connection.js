02336b747d2bae281998ad4f8f7c99d14a4fc444
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-08-27 19:51:32 -0700

    updated sql file and rendered hbs

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,3 @@
 const mysql = require('mysql2')
 
-module.exports = mysql.createConnection(process.env.JAWSDB_URL || 'mysql://root:Javascript${}535@localhost/grocery_db')
+module.exports = mysql.createConnection(process.env.JAWSDB_URL || 'mysql://root:Javascript${}535@localhost/burger_db')
--- db/seed.sql
+++ db/seed.sql
@@ -1,6 +1,6 @@
 USE burger_db;
 
-INSERT INTO burger (burger_name, devoured)
+INSERT INTO burgers (burger_name, devoured)
 VALUES 
 ('Jalapeno Western Bacon', false),
 ('Big Avo-Burg', false),
